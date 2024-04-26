ec7d41d30be00f8c9999e913d875bf3d097ba2fc
git <duyongsheng@genmesce.com>
2024-04-26 16:54:17 +0800

    判断serface为非null

--- libausbc/src/main/java/com/jiangdg/ausbc/MultiCameraClient.kt
+++ libausbc/src/main/java/com/jiangdg/ausbc/MultiCameraClient.kt
@@ -455,7 +455,9 @@ class MultiCameraClient(ctx: Context, callback: IDeviceConnectCallBack?) {
                     mUvcCamera?.setPreviewDisplay(cameraView.holder)
                 }
                 is TextureView -> {
-                    mUvcCamera?.setPreviewTexture(cameraView.surfaceTexture)
+                    if(cameraView.surfaceTexture != null){
+                        mUvcCamera?.setPreviewTexture(cameraView.surfaceTexture)
+                    }
                 }
                 else -> {
                     throw IllegalStateException("Only support Surface or SurfaceTexture or SurfaceView or TextureView or GLSurfaceView--$cameraView")
@@ -492,18 +494,18 @@ class MultiCameraClient(ctx: Context, callback: IDeviceConnectCallBack?) {
                 if (! CameraUtils.hasStoragePermission(ctx)) {
                     mMainHandler.post { callback.onError("have no storage permission") }
                     Logger.e(TAG ,"open camera failed, have no storage permission")
-                   // return@submit
+                    return@submit
                 }
                 if (! isPreviewed || mPreviewSize == null) {
                     mMainHandler.post { callback.onError("camera not previewing") }
                     Logger.i(TAG, "captureImageInternal failed, camera not previewing")
-                    //return@submit
+                    return@submit
                 }
                 val data = mNV21DataQueue.pollFirst(CAPTURE_TIMES_OUT_SEC, TimeUnit.SECONDS)
                 if (data == null) {
                     mMainHandler.post { callback.onError("Times out") }
                     Logger.i(TAG, "captureImageInternal failed, times out.")
-                    //return@submit
+                    return@submit
                 }
                 mMainHandler.post { callback.onBegin() }
                 val date = mDateFormat.format(System.currentTimeMillis())
@@ -547,7 +549,7 @@ class MultiCameraClient(ctx: Context, callback: IDeviceConnectCallBack?) {
                     callBack.onError("have no storage or audio permission")
                 }
                 Logger.e(TAG ,"open camera failed, have no storage and audio permission")
-                //return
+                return
             }
             mMediaMuxer = Mp4Muxer(ctx, callBack, path, durationInSec)
             (mVideoProcess as? H264EncodeProcessor)?.apply {
