8c7e5cb897693abec36e1612bb80b51773f2edd3
git <duyongsheng@genmesce.com>
2024-08-19 23:23:38 +0800

    修改hue底层接口

--- libausbc/src/main/java/com/jiangdg/ausbc/MultiCameraClient.kt
+++ libausbc/src/main/java/com/jiangdg/ausbc/MultiCameraClient.kt
@@ -527,23 +527,13 @@ class MultiCameraClient(ctx: Context, callback: IDeviceConnectCallBack?) {
                     return@submit
                 }
                 val values = ContentValues()
-                if(Build.VERSION.SDK_INT >= 29){
-                    values.put(MediaStore.Images.ImageColumns.DISPLAY_NAME, displayName)
-                    values.put(MediaStore.Images.ImageColumns.DATE_ADDED, System.currentTimeMillis() / 1000)
-                    values.put(MediaStore.Images.ImageColumns.DATE_TAKEN, date)
-                    values.put(MediaStore.Images.ImageColumns.IS_PENDING, true)
-                    values.put(MediaStore.Images.ImageColumns.LONGITUDE, location?.longitude)
-                    values.put(MediaStore.Images.ImageColumns.LATITUDE, location?.latitude)
-                }
-                else{
-                    values.put(MediaStore.Images.ImageColumns.TITLE, title)
-                    values.put(MediaStore.Images.ImageColumns.DISPLAY_NAME, displayName)
-                    values.put(MediaStore.Images.ImageColumns.DATE_ADDED, System.currentTimeMillis() / 1000)
-//                    values.put(MediaStore.Images.ImageColumns.DATA, path)
-//                    values.put(MediaStore.Images.ImageColumns.DATE_TAKEN, date)
-                    values.put(MediaStore.Images.ImageColumns.LONGITUDE, location?.longitude)
-                    values.put(MediaStore.Images.ImageColumns.LATITUDE, location?.latitude)
-                }
+                values.put(MediaStore.Images.ImageColumns.TITLE, title)
+                values.put(MediaStore.Images.ImageColumns.DISPLAY_NAME, displayName)
+                values.put(MediaStore.Images.ImageColumns.DATE_ADDED, System.currentTimeMillis() / 1000)
+                values.put(MediaStore.Images.ImageColumns.DATA, path)
+                values.put(MediaStore.Images.ImageColumns.DATE_TAKEN, date)
+                values.put(MediaStore.Images.ImageColumns.LONGITUDE, location?.longitude)
+                values.put(MediaStore.Images.ImageColumns.LATITUDE, location?.latitude)
                 ctx.contentResolver?.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
                 mMainHandler.post { callback.onComplete(path) }
                 if (Utils.debugCamera) { Logger.i(TAG, "captureImageInternal save path = $path") }
--- libuvc/src/main/java/com/serenegiant/usb/UVCCamera.java
+++ libuvc/src/main/java/com/serenegiant/usb/UVCCamera.java
@@ -832,11 +832,13 @@ public class UVCCamera {
      * @param hue [%]
      */
 	public synchronized void setHue(final int hue) {
-    	if (mNativePtr != 0) {
- 		   final float range = Math.abs(mHueMax - mHueMin);
- 		   if (range > 0)
- 			   nativeSetHue(mNativePtr, (int)(hue / 100.f * range) + mHueMin);
-    	}
+		nativeSetHue(mNativePtr, hue);
+
+//    	if (mNativePtr != 0) {
+// 		   final float range = Math.abs(mHueMax - mHueMin);
+// 		   if (range > 0)
+// 			   nativeSetHue(mNativePtr, (int)(hue / 100.f * range) + mHueMin);
+//    	}
     }
 
     /**
@@ -844,15 +846,17 @@ public class UVCCamera {
      * @return hue[%]
      */
 	public synchronized int getHue(final int hue_abs) {
-	   int result = 0;
-	   if (mNativePtr != 0) {
-		   nativeUpdateHueLimit(mNativePtr);
-		   final float range = Math.abs(mHueMax - mHueMin);
-		   if (range > 0) {
-			   result = (int)((hue_abs - mHueMin) * 100.f / range);
-		   }
-	   }
-	   return result;
+		return hue_abs;
+
+//	   int result = 0;
+//	   if (mNativePtr != 0) {
+//		   nativeUpdateHueLimit(mNativePtr);
+//		   final float range = Math.abs(mHueMax - mHueMin);
+//		   if (range > 0) {
+//			   result = (int)((hue_abs - mHueMin) * 100.f / range);
+//		   }
+//	   }
+//	   return result;
 	}
 
     /**
